668e2f7955febfd0a1ccbdaede8aff3a098c4e12
Miguel Savignano <migue.masx@gmail.com>
2017-08-09 09:19:54 +0200

    ok

--- client/app.js
+++ client/app.js
@@ -1,12 +1,12 @@
 var complex_query = { q: { age: 24, "$or" : [{ name: "Robert" }]}, order: { name: "desc" } }
-const fetchConfig = {
-  headers:{
-    "Content-Type": "application/json",
-  }
-}
 var getPeople = async (complex_query) => {
   var query = $.param(complex_query)
-  var response = await fetch(`http://localhost:3000/people?${query}`, fetchConfig)
+  var response = await fetch(`http://localhost:3000/people?${query}`, {
+    method: "GET",
+    headers:{
+      "Content-Type": "application/json",
+    },
+  })
   var json = await response.json()
   console.log("People")
   console.log(json)
@@ -15,10 +15,10 @@ var getPeople = async (complex_query) => {
 const sigIn = async () => {
   var response = await fetch(`http://localhost:3000/user_token`, {
     method: "POST",
+    body: JSON.stringify({"auth": {"email": "user@test.com", "password": "12345678"}}),
     headers:{
-      "Content-Type": "application/json",
+      "Content-Type": "application/json"
     },
-    body: JSON.stringify({"auth": {"email": "user@test.com", "password": "12345678"}})
   })
 }
 sigIn()
