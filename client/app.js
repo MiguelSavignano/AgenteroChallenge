acf12929743f75a704049cf767e2b395b6b2c2e2
Miguel Savignano <migue.masx@gmail.com>
2017-08-09 13:13:29 +0200

    client with sig_in example

--- client/app.js
+++ client/app.js
@@ -1,25 +1,27 @@
 var complex_query = { q: { age: 24, "$or" : [{ name: "Robert" }]}, order: { name: "desc" } }
+
 var getPeople = async (complex_query) => {
   var query = $.param(complex_query)
+  const token = await sigIn()
   var response = await fetch(`http://localhost:3000/people?${query}`, {
-    method: "GET",
-    headers:{
-      "Content-Type": "application/json",
-    },
+    headers: {'Authorization': `Bearer ${token}`},
   })
-  var json = await response.json()
+  const json = await response.json()
   console.log("People")
   console.log(json)
 }
 
-const sigIn = async () => {
-  var response = await fetch(`http://localhost:3000/user_token`, {
-    method: "POST",
-    body: JSON.stringify({"auth": {"email": "user@test.com", "password": "12345678"}}),
-    headers:{
-      "Content-Type": "application/json"
+const sigIn = async (email = "user@test.com", password = "12345678") => {
+  var response = await fetch('http://localhost:3000/user_token', {
+    method: "post",
+    body: JSON.stringify({"auth": { email, password }}),
+    headers: {
+      "Content-Type": "application/json",
+      'Accept': 'application/json',
     },
   })
+  var json = await response.json()
+  return json.jwt
 }
-sigIn()
-// getPeople(complex_query)
+// sigIn()
+getPeople(complex_query)
