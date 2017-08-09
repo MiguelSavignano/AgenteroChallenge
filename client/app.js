a8948e3ff62f392f0f0bd292b2b917b2bde80979
Miguel Savignano <migue.masx@gmail.com>
2017-08-09 13:55:59 +0200

    app example with querys and sig in

--- client/app.js
+++ client/app.js
@@ -1,13 +1,16 @@
-var complex_query = { q: { age: 29 }, order: { name: "desc" } }
+var complex_query1 = { q: { age: 29, oss_projects: 3 }, order: {age: "desc"} }
+var complex_query2 = { q: { "$or": [{ name: "John" }, { name: "Amanda" }] } }
+var complex_query3 = { q: { age: 24, "$or": [{ name: "Robert" }]}, order: { name: "desc" } }
+var complex_query4 = { q: {"age": {"$gte": 25 }}, order: { age: "desc" } }
 
-var getPeople = async (complex_query) => {
+var getPeople = async (complex_query, token) => {
   var query = $.param(complex_query)
-  const token = await sigIn()
   var response = await fetch(`http://localhost:3000/people?${query}`, {
     headers: {'Authorization': `Bearer ${token}`},
   })
   const json = await response.json()
-  console.log("People")
+  console.log("GET /people")
+  console.log("params: ", JSON.stringify(complex_query))
   console.log(json)
 }
 
@@ -23,5 +26,12 @@ const sigIn = async (email = "user@test.com", password = "12345678") => {
   var json = await response.json()
   return json.jwt
 }
-// sigIn()
-getPeople(complex_query)
+
+const init = async () => {
+  const token = await sigIn()
+  getPeople(complex_query1, token)
+  getPeople(complex_query2, token)
+  getPeople(complex_query3, token)
+  getPeople(complex_query4, token)
+}
+init()
